921d9315c8485c54d30ca7fb94167b2eedc1e469
notsoold <1000lop@gmail.com>
2017-03-24 18:41:45 +0300

    Added user commands input; everything worked just fine

--- snbash.c
+++ snbash.c
@@ -8,65 +8,130 @@
 #include <stdio.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
-int main(int argc, char *argv[])
+#define BUFSIZE 512
+
+int main(void)
 {
 	pid_t pid;
-	pid_t terminalPid;
+	//pid_t terminalPid;
 	int exitCode;
 	int i = 0;
 	
-	terminalPid = fork();
+	/*terminalPid = fork();
 	if(terminalPid == -1)
 	{
-		printf("Opening log terminal failed. Stopping...\n");
+		printf("Opening log terminal failed. Stopping.\n");
 		perror(NULL);
 		exit(2);
 	}
 	else if(terminalPid == 0)
 	{
 		execlp("gnome-terminal", "gnome-terminal", NULL);
-	}
+	}*/
 	
-	char *exec_args[argc];
-	for(; i < argc-1; i++)
+	while(1)
 	{
-		exec_args[i] = argv[i+1];
+		sleep(1);
+		char str[BUFSIZE];
+		getcwd(str, BUFSIZE);
+		strcat(str, ": ");
+		write(1, str, strlen(str));
+		char argstr[BUFSIZE];
+		for(i = 0; i < BUFSIZE; i++)
+			argstr[i] = '\0';
+		i = 0;
 		
-	}
-	exec_args[i] = NULL;
-	pid = fork();
-	switch(pid)
-	{
-		case -1:
-			printf("Forking failed.\n");
-			perror(NULL);
+		int readed = read(0, argstr, BUFSIZE);
+		if(readed == 0 || strcmp(argstr, "\n") == 0)
+			continue;
+		else if(strcmp(argstr, "exit\n") == 0)
 			break;
-		case 0:
-			execvp(exec_args[0], exec_args);
-			exitCode = 0;
-			break;
-		default:
-			exitCode = 1;
-			break;
-	}
 	
-	if(pid != 0)
-	{
-		int stat_val;
-		pid_t child_pid;
-		child_pid = wait(&stat_val);
-		printf("\nChild has finished\n");
-		if(WIFEXITED(stat_val))
+		for(i = strlen(argstr)-1; i >= 0; i--)
 		{
-			printf("Child exited with code %d\n", WEXITSTATUS(stat_val));
+			if(isalnum(argstr[i]) || ispunct(argstr[i]))
+				break;
+			if(argstr[i] == ' ')
+			{
+				argstr[i] = '\0';
+			}
+			else if(argstr[i] == '\n')
+			{
+				argstr[i] = '\0';
+			}
 		}
-		else
+	
+		int cnt = 0;
+		i = 0;
+		do
 		{
-			printf("Child terminated abnormally. o.O\n");
-			perror(NULL);
+			if(argstr[i] == ' ')
+				cnt++;
+			i++;
+		} while(argstr[i-1] != '\0');
+	
+		char *args[cnt+3];
+		i = 0;
+		args[i] = strtok(argstr, " ");
+		i++;
+		while(args[i-1] != NULL)
+		{
+			args[i] = strtok(NULL, " ");
+			i++;
+		}
+		
+		if(strcmp(args[0], "cd") == 0)
+		{
+			chdir(args[1]);
+			continue;
+		}
+	
+		// That is how I tried to do... Nothing works.
+		//char *logto = (char *)calloc(BUFSIZE, 1);
+		//sprintf(logto, ">/proc/%d/fd/1", terminalPid);
+		//sprintf(logto, ">/dev/pts/6");
+		//sprintf(logto, ">>outlog.txt");
+		//args[cnt+1] = logto;
+		//args[cnt+2] = NULL;
+		
+		for(i = 0; i < cnt+3; i++)
+			printf("arg %d: %s\n", i, args[i]);
+		i = 0;
+
+		pid = fork();
+		switch(pid)
+		{
+			case -1:
+				printf("Forking failed.\n");
+				perror(NULL);
+				break;
+			case 0:
+				execvp(args[0], args);
+				exitCode = 0;
+				break;
+			default:
+				exitCode = 1;
+				break;
+		}
+	
+		if(pid != 0)
+		{
+			int stat_val;
+			pid_t child_pid;
+			child_pid = wait(&stat_val);
+			if(WIFEXITED(stat_val))
+			{
+				printf("Operation finished with code %d\n", WEXITSTATUS(stat_val));
+			}
+			else
+			{
+				printf("Child terminated abnormally.\n");
+				perror(NULL);
+			}
 		}
 	}
-	
 	exit(exitCode);
 }
